c0fb54d27a7b902c8933b70dfaaa9c34024f0dd8
maraputri <maraputri61@gmail.com>
2026-05-24 12:01:01 +0700

    menambahkan LoginController dan ProductsController

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('home');
+// public routes
+Route::get('/', function () { return view('home'); })->name('home');
+Route::get('/about', function () { return view('about'); })->name('about');
+Route::get('/contact', function () { return view('contact'); })->name('contact');
+
+
+// Authentication routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// admin and users routes protected by auth middleware
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+     })->name('admin.dashboard');
+
+    // User management routes
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users');
+    Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
+    Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+    Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
+    Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
+    Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
+
+    // Product management routes
+    Route::get('/admin/products', [ProductsController::class, 'index'])->name('admin.products');
+    Route::get('/admin/products/create', [ProductsController::class, 'create'])->name('admin.products.create');
+    Route::post('/admin/products', [ProductsController::class, 'store'])->name('admin.products.store');
+    Route::get('/admin/products/{product}/edit', [ProductsController::class, 'edit'])->name('admin.products.edit');
+    Route::put('/admin/products/{product}', [ProductsController::class, 'update'])->name('admin.products.update');
+    Route::delete('/admin/products/{product}', [ProductsController::class, 'destroy'])->name('admin.products.destroy');
+
 });
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-});
-
-Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users');
-Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
-Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
-Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
-Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
-Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
